8e2faf0f6890aceea95e08e27a301515b7b06dbe
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-02-16 17:30:15 -0500

    update html title

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,6 @@ class App extends Component {
   render () {
     return (
       <>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
         <h1>Converting from Function to Class Component</h1>
         <div className="card">
           <button>
