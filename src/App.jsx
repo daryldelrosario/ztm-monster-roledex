1259818fdf8c40dad11882d2c01616b22ea94318
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-02-22 22:50:55 -0500

    complete basic optimisation

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,20 @@ class App extends Component {
       }))
   }
 
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
   render () {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -36,13 +47,7 @@ class App extends Component {
           className='search-box' 
           type='search' 
           placeholder='Search Monsters' 
-          onChange={(e) => {
-            const searchField = e.target.value.toLocaleLowerCase();
-
-            this.setState(() => {
-              return { searchField };
-            })
-          }}
+          onChange={onSearchChange}
         />
 
         {filteredMonsters.map((monster) => {
