8580a198242d839b1e13110f7cbff12c79617e2e
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-02-23 20:38:33 -0500

    complete basic cardlist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -50,6 +52,7 @@ class App extends Component {
           onChange={onSearchChange}
         />
 
+        <CardList />
         {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -1,1 +1,14 @@
 import { Component } from 'react';
+
+class CardList extends Component {
+  
+  render() {
+    return (
+      <>
+        <h1>This is the CardList Component</h1>
+      </>
+    )
+  }
+}
+
+export default CardList;
