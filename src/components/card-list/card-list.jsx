7fdca9bb332424eab51f2692c194583d6992d07d
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-02-27 22:40:13 -0500

    complete basic card-list component

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -7,8 +7,18 @@ class CardList extends Component {
 
     return (
       <>
-        {monsters.map(monster => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className='card-container' key={id}>
+              <img 
+                src={`https://robohash.org/${id}?set=set2&size=180x180`} 
+                alt={`monster ${name}`} 
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
         })}
       </>
     )
